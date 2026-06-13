a91c579a268eccf66e9366567a3ca7f521bceeee
Herdwick <theohill2022@gmail.com>
2026-06-13 00:54:20 -0400

    Got the addition and subtraction logic down
    Super basic input and output calculator so far. I'm pretty sure there is a bunch of logic stuff I could use. I'm still learning.

--- Algebra_add_sub.cpp
+++ Algebra_add_sub.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 float a;
 float b;
+string op;
 
 int main()
 {
@@ -16,16 +17,59 @@ int main()
 
     */
     cin >> a;
-      cout<< "Input entered: " << a;
-
+      cout<< "User input: " << a;
+            cout << " " << endl;
+            cout << " " << endl;
       cout<< "input the second number: " << endl;
       cin >> b;
-      cout<< "Input entered: " << b;
+      cout<< "User input: " << b;
+            cout << " " << endl;
+            cout << " " << endl;
+      cout<< "choose an operator. Ex 'plus or 'minus' in lower case letters" << endl;
+      cin >> op;
+      cout<< "User input: " << op;
+            cout << " " << endl;
+            cout << " " << endl;
+      string answer;
+      if (op == "plus"){
+        cout << "Did you mean to pick 'plus'? 'Y' / 'N' " << endl;
+        cin >> answer; 
+            if (answer == "Y"){
+                float plus = a + b;
+                cout << a << " + " << b << " = " << plus << endl;
+               
+            }
+            else if (answer == "N"){
+                cout << "Please restart. I have forgotten" << endl; //Maybe make it loop back to the start?
+            }
+            else
+            {
+                cout<< "BRUH. PLEASE USE THE RIGHT INPUT. I'M NEW AT THIS" << endl; // NGL They might need to restart after this
+            }
 
-      cout<< "choose an operator: + or - " << endl;
-      //cin >> operator;
-        // Do a logic thingy where if the user put in a plus or minus it outputs the equation and the answer.
-
-
+   
+        }
+     else if (op == "minus"){
+           cout << "Did you mean to pick 'minus'? 'Y' / 'N' " << endl; /* basically everything below is copy/paste */
+            cin >> answer; 
+                if (answer == "Y"){
+                    float minus = a - b;
+                    cout << a << " - " << b << " = " << minus << endl;
+                
+                }
+                else if (answer == "N"){
+                    cout << "Please restart. I have forgotten" << endl;
+                }
+                else
+                {
+                    cout<< "BRUH. PLEASE USE THE RIGHT INPUT. I'M NEW AT THIS" << endl;
+                }
+    }
+    else{
+        cout << "ERROR: Please try again. Check spelling or capitalization" << endl;
+    }
+            cout << " " << endl;
+            cout << " " << endl;
+    cout << "Thank you, user! I hope you found the answer you werse looking for!"<< endl;
     return (0);
 }
